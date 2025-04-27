0779b593c091549b74361506a121572c75fd6b0a
petrovyuri <y.petrov@friflex.com>
2025-04-27 19:47:59 +0300

    chore(out): delete out folder

--- out/commands/create_files/creators/assd.js
+++
@@ -1,8 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-exports.createStatelessWidget = void 0;
-const createStatelessWidget = async (uri) => {
-    vscode.window.showErrorMessage('Необходимо ввести название модуля');
-};
-exports.createStatelessWidget = createStatelessWidget;
-//# sourceMappingURL=assd.js.map
